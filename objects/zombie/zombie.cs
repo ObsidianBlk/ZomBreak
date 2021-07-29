1c536881ac1bfc8655afc126d36773302c4a2a48
Bryan Miller <bmiller1008@gmail.com>
2021-07-28 20:02:32 -0400

    Small (technical) improvement to zombie walk. Zombie will die if player enters within a radius.

--- objects/zombie/zombie.cs
+++ objects/zombie/zombie.cs
@@ -17,11 +17,11 @@ public class zombie : KinematicBody
   private STATE state = STATE.IDLE;
 
   private float min_walk_distance = 1f;
-  private float max_walk_distance = 4f;
+  private float max_walk_distance = 2f;
   private float walk_speed = 4f;
+  private float turn_rate = 90f; // Degrees per second
 
-  private Vector3 target_position = new Vector3(0f,0f,0f);
-
+  private Vector3 target_position = new Vector3(0f,0f,0f); 
 
   /* ------------------------------------------------------------
    * Public Properties
@@ -51,7 +51,7 @@ public class zombie : KinematicBody
 
   /* ------------------------------------------------------------
    * Private Methods
-   * ----------------------------------------------------------*/
+   * ----------------------------------------------------------*/ 
 
   private void ProcessState_Idle(float delta){
     if (anim.CurrentAnimation != "Idle")
@@ -60,30 +60,72 @@ public class zombie : KinematicBody
     if (rand.Next(100) > 75){
       GD.Print("I'm gonna walk");
       float walk_distance = min_walk_distance + ((max_walk_distance - min_walk_distance) * (float)rand.NextDouble());
-      target_position = (new Vector3(
-          (float)rand.NextDouble(),
-          GlobalTransform.origin.y,
-          (float)rand.NextDouble()
-      )).Normalized() * walk_distance;
+      target_position = GlobalTransform.origin + ((new Vector3(
+          (float)rand.NextDouble() - 0.5f,
+          0f,
+          (float)rand.NextDouble() - 0.5f
+      )).Normalized() * walk_distance);
       state = STATE.WANDER;
     } else {
       GD.Print("I'm good right here.");
     }
   }
 
+  private float D2R(float d){
+    return ((float)(Math.PI/180.0))*d;
+  }
+
+  private float Sign(float v){
+    if (v >= 0f){return 1f;}
+    return -1f;
+  }
+
   private void ProcessState_Wander(float delta){
     if (anim.CurrentAnimation != "Walking")
       anim.Play("Walking");
-    float target_angle = GlobalTransform.basis.z.AngleTo(target_position); 
-    GlobalTransform = GlobalTransform.Rotated(GlobalTransform.basis.y, target_angle * delta);
+    Vector3 dir = GlobalTransform.origin.DirectionTo(target_position);
+    float d = (GlobalTransform.basis.z).Dot(dir);
+    float target_angle = D2R(180f) * d;
+    float aps = D2R(Sign(d) * turn_rate) * delta;
+    if (Math.Abs(aps) > Math.Abs(target_angle)){
+      aps = 0f;
+    }
+    //GD.Print("APS: ", aps, " | Angle To: ", target_angle, " | Rot: ", Rotation.y);
+
+
+    Rotate(new Vector3(0f, 1f, 0f), aps);
+    //GD.Print("Rot: ", Rotation);
+    //GlobalTransform = GlobalTransform.Rotated(GlobalTransform.basis.y, target_angle * delta);
     MoveAndSlide(GlobalTransform.basis.z * walk_speed * delta);
-    if (GlobalTransform.origin.DistanceTo(target_position) < 0.5f){
+    float dist = DistanceTo2D();
+    //GD.Print("From: ", GlobalTransform.origin, " | To: ", target_position);
+    //GD.Print("Distance To: ", dist);
+    if (dist < 0.1f){
       state = STATE.IDLE;
     }
   }
 
   private void ProcessState_Death(float delta){
+    if (anim.CurrentAnimation != "Death")
+      anim.Play("Death");
+  }
 
+  private float DistanceTo2D(){
+    Vector2 from = new Vector2(
+      GlobalTransform.origin.x,
+      GlobalTransform.origin.z
+    );
+    Vector2 to = new Vector2(
+      target_position.x,
+      target_position.z
+    );
+    //GD.Print("From: ", from, " | To: ", to);
+    return from.DistanceTo(to);
+  }
+
+  private float AngleOf(Vector3 v){
+    Vector2 _v = new Vector2(v.x, v.z);
+    return _v.Angle();
   }
 
   /* ------------------------------------------------------------
@@ -99,6 +141,12 @@ public class zombie : KinematicBody
       parent.RemoveChild(this);
       QueueFree();
     }
+  }
+
+
+  public void _on_body_entered(Node body){
+    if (body.IsInGroup("player"))
+      state = STATE.DEATH;
   }
 }
 
